6fa9dd66dad529de23582c914315139a6f7d1d86
mhmtkarakas <m.karakas32@gmail.com>
2024-06-25 15:10:10 +0300

    Progress

--- materialui/src/App.tsx
+++ materialui/src/App.tsx
@@ -28,6 +28,7 @@ import { LessonToolTip } from "./components/LessonToolTip";
 import { LessonAlert } from "./components/LessonAlert";
 import { LessonDialog } from './components/LessonDialog';
 import { LessonSnackbar } from "./components/LessonSnackbar";
+import { LessonProgress } from './components/LessonProgress';
 
 function App() {
   return (
@@ -59,7 +60,8 @@ function App() {
       {/* <LessonToolTip /> */}
       {/* <LessonAlert /> */}
       {/* <LessonDialog /> */}
-      <LessonSnackbar />
+      {/* <LessonSnackbar /> */}
+      <LessonProgress />
     </div>
   );
 }
